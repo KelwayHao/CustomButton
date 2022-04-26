c980f6816efe0666e7a21ed2543162bf26f4eadd
KelwayHao <kelwaycf@gmail.com>
2022-04-26 16:22:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/custombutton/CustomButton.kt
@@ -0,0 +1,12 @@
+package com.example.custombutton
+
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.util.AttributeSet
+import android.view.View
+
+class CustomButton(context: Context, attributeSet: AttributeSet): View(context, attributeSet) {
+
+ 
+}
